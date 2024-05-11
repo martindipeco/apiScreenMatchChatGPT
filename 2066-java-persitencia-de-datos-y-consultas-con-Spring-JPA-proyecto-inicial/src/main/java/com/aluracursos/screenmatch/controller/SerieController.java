02560215658d2fd4e104fff40eb57f7df58d24c4
martindipeco <martindipeco@gmail.com>
2024-05-10 21:10:46 -0300

    migrando a un MVC web

---
+++ 2066-java-persitencia-de-datos-y-consultas-con-Spring-JPA-proyecto-inicial/src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -0,0 +1,13 @@
+package com.aluracursos.screenmatch.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SerieController {
+    @GetMapping("/series")
+    public String mostrarMensaje()
+    {
+        return "Hola mundo Spring";
+    }
+}

d45d05e7437e94e62aa83c2ae120d308564caf09
martindipeco <martindipeco@gmail.com>
2024-05-11 12:02:56 -0300

    serie controller para que devuelva lista de series

--- 2066-java-persitencia-de-datos-y-consultas-con-Spring-JPA-proyecto-inicial/src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
+++ 2066-java-persitencia-de-datos-y-consultas-con-Spring-JPA-proyecto-inicial/src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -1,13 +1,25 @@
 package com.aluracursos.screenmatch.controller;
 
+import com.aluracursos.screenmatch.model.Serie;
+import com.aluracursos.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class SerieController {
+
+    @Autowired //inyecto dependencia
+    private SerieRepository repository;
     @GetMapping("/series")
-    public String mostrarMensaje()
+    public List<Serie> obtenerTodasLasSeries()
     {
-        return "Hola mundo Spring";
+        return repository.findAll();
     }
+//    public String mostrarMensaje()
+//    {
+//        return "Hola mundo Spring";
+//    }
 }

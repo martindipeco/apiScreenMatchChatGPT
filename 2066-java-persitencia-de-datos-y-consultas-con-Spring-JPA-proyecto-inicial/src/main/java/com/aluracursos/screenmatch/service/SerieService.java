9cf6de2c2857820ae1b9df5fc172a1147a1e6b99
martindipeco <martindipeco@gmail.com>
2024-05-12 18:01:03 -0300

    agregando funcionalidades y refactoreos top 5 y mas recientes

--- 2066-java-persitencia-de-datos-y-consultas-con-Spring-JPA-proyecto-inicial/src/main/java/com/aluracursos/screenmatch/service/SerieService.java
+++ 2066-java-persitencia-de-datos-y-consultas-con-Spring-JPA-proyecto-inicial/src/main/java/com/aluracursos/screenmatch/service/SerieService.java
@@ -1,6 +1,7 @@
 package com.aluracursos.screenmatch.service;
 
 import com.aluracursos.screenmatch.dto.SerieDTO;
+import com.aluracursos.screenmatch.model.Serie;
 import com.aluracursos.screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,8 +16,24 @@ public class SerieService {
     private SerieRepository repository;
     public List<SerieDTO> obtenerTodasLasSeries()
     {
-        return repository.findAll().stream()
+        return convierteDatos(repository.findAll());
+    }
+
+    public List<SerieDTO> obtenerTop5()
+    {
+        return convierteDatos(repository.findTop5ByOrderByEvaluacionDesc());
+    }
+
+    public List<SerieDTO> obtenerMasRecientes()
+    {
+        return convierteDatos(repository.lanzamientosMasRecientes());
+    }
+
+    public List<SerieDTO> convierteDatos(List<Serie> serie)
+    {
+        return serie.stream()
                 .map(s -> new SerieDTO(s.getTitulo(), s.getTotalTemporadas(), s.getEvaluacion(), s.getPoster()
                         , s.getGenero(), s.getActores(), s.getSinopsis())).collect(Collectors.toList());
     }
+
 }
